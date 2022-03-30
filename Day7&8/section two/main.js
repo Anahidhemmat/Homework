fe891ad9f4903162e1f2c2313c5c6d5879c47867
Anahid <anahidhemmat2130@gmail.com>
2022-03-30 16:07:09 +0400

    drag & drop functionality

--- Day7&8/section two/main.js
+++ Day7&8/section two/main.js
@@ -36,11 +36,56 @@ function createList() {
         <span class="number">${index + 1}</span>
         <div class="draggable" draggable="true">
         <p class="person-name">${person}</p>
-        <i class="fas fa-grid-lines"></i>
+        <i class="fa-solid fa-grip-lines"></i>
         </div>
         `;
 
       listItems.push(listItem);
       draggable_list.appendChild(listItem);
     });
+
+  addEventListeners();
+}
+
+// drag listeners
+function dragStart() {
+  dragStartIndex = this.closest("li").getAttribute("data-index");
+  console.log(dragStartIndex);
+}
+function dragLeave() {
+  this.classList.remove("over");
+}
+function dragOver(e) {
+  e.preventDefault();
+}
+function dragDrop() {
+  const dragEndIndex = this.getAttribute("data-index");
+  swapItems(dragStartIndex, dragEndIndex);
+
+  this.classList.remove("over");
+}
+function swapItems(fromIndex, toIndex) {
+  //   console.log(123);
+  const itemOne = listItems[fromIndex].querySelector(".draggable");
+  const itemTwo = listItems[toIndex].querySelector(".draggable");
+  listItems[fromIndex].appendChild(itemTwo);
+  listItems[toIndex].appendChild(itemOne);
+}
+function dragEnter() {
+  this.classList.add("over");
+}
+
+function addEventListeners() {
+  const draggables = document.querySelectorAll(".draggable");
+  const dragListItems = document.querySelectorAll(".draggable-list li");
+  draggables.forEach((draggable) => {
+    draggable.addEventListener("dragstart", dragStart);
+  });
+
+  dragListItems.forEach((item) => {
+    item.addEventListener("dragover", dragOver);
+    item.addEventListener("drop", dragDrop);
+    item.addEventListener("dragenter", dragEnter);
+    item.addEventListener("dragleave", dragLeave);
+  });
 }
